1c0afbd696218fb1ce19b08a7ca2381e5797a37a
Henry Harris <henrythomasharris@gmail.com>
2020-08-20 18:40:24 -0400

    Better flow between search and cancel button

--- ios/plate-mate/plate-mate/PlateList.swift
+++ ios/plate-mate/plate-mate/PlateList.swift
@@ -14,28 +14,32 @@ struct PlateList: View {
     
     var body: some View {
         VStack {
-           HStack {
-               TextField("Search...", text: $text)
-                   .padding(7)
-                   .padding(.horizontal, 25)
-                   .background(Color(.systemGray6))
-                   .cornerRadius(8)
-                   .padding(.horizontal, 10)
-                   .onTapGesture {
-                       self.isEditing = true
-                   }
+            HStack {
+                TextField("Search...", text: $text)
+                    .padding(2.5)
+                    .padding(.horizontal, 10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(6)
+                    .padding(.leading, 10)
+                    .padding(.trailing, isEditing ? 0 : 10)
+                    .onTapGesture {
+                        self.isEditing = true
+                    }
+                    .transition(.move(edge: .trailing))
+                    .animation(.default)
     
-               if isEditing {
-                   Button(action: {
-                       self.isEditing = false
-                       self.text = ""
-    
-                   }) {
-                       Text("Cancel")
-                   }
-                   .padding(.trailing, 10)
-                   .transition(.move(edge: .trailing))
-                   .animation(.default)
+                if isEditing {
+                    Button(action: {
+                        self.isEditing = false
+                        self.text = ""
+                        self.hideKeyboard()
+                    }) {
+                        Text("Cancel")
+                    }
+                    .padding(.trailing, 10)
+                    .padding(.leading, 0)
+                    .transition(.move(edge: .trailing))
+                    .animation(.default)
                }
             }
             
@@ -46,6 +50,17 @@ struct PlateList: View {
                     }
             }
         }
+        .onTapGesture {
+            // TODO: clear keyboard, maybe other stuff?
+            self.isEditing = false
+            self.hideKeyboard()
+        }
+        // TODO: set actual game title
+        .navigationBarTitle("Game Title")
+    }
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
